274eebc8ce54876fb5cc05ca20a7a76b16996ecd
makoeppel <koeppel.ma@googlemail.com>
2017-08-20 12:09:50 +0200

    add footer, links and title picture

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,17 @@ $(document).ready(function() {
 		removeClasses();
 		$('.introText').fadeIn();
 	});
+	$('.introBox1').click(function(){
+		window.location.href = "http://test-monika.s-and-k.de/index.php?id_cms=7&controller=cms";
+	});
+	$('.introBox2').click(function(){
+		window.location.href = "http://test-monika.s-and-k.de/index.php?id_cms=8&controller=cms";
+	});
+	$('.introBox3').click(function(){
+		window.location.href = "http://test-monika.s-and-k.de/index.php?controller=contact";
+	});
 	$('.introBox4').click(function(){
-		window.location.href = "http://shop.monikashaarstudio.de/";
+		window.location.href = "http://test-monika.s-and-k.de/index.php?id_category=12&controller=category";
 	});
 
 });
